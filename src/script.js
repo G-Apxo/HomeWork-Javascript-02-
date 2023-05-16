09156d92cdb72e491a5efdd0f41dbbd5067aa4a7
Giorgi Apkhadze <giorgi@rad.inc>
2023-05-16 20:16:49 +0400

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,65 +1,71 @@
 /**ერთგანზომილებიაი მასივი**/
 let peole = ["nino", "eto", "ketino"];
 let numbers = [
-    [1,2,3],
-    [4,5,6],
+  [1, 2, 3],
+  [4, 5, 6],
 ];
 console.log(numbers[0][2]);
 console.log(numbers[1][2]);
 
-let people = ["nino", "eto", "ketino"]
+let people = ["nino", "eto", "ketino"];
 console.log(people);
 
-for (let i = 0; 1 < people.lenght; i++){
-    document.write(people[i]+ "<br>")
-} 
-for (index in people){
-  document.write(people[index] + "<br>")
+for (let i = 0; 1 < people.lenght; i++) {
+  document.write(people[i] + "<br>");
+}
+for (index in people) {
+  document.write(people[index] + "<br>");
 }
 /**დასრულდა **/
 
 /**kenti ricxvebi **/
-const number= [15,53,22,198,10,28,16,70,33,951]
- for (let i = 0; i < number.length; i++){
-  if ([number[i] > 10 ] ) break ;
-  console.log(number[i]);
- }
+const number = [15, 53, 22, 198, 10, 28, 16, 70, 33, 951];
+for (let i = 0; i < number.length; i++) {
+  if (number[i] % 2 !== 0) {
+    console.log("kenti", number[i]);
+  }
+}
 /*kentiricxvebi dasasruli** */
 
 /**function*/
-for(let i = 0; i < number.length; i++){
-  if (number[i] > 77) break;
-  console.log(number[i]);
+for (let i = 0; i < number.length; i++) {
+  if (number[i] > 77) {
+    console.log(number[i]);
+  }
 }
-function lecture(){
-    document.write("lecture");
+function lecture() {
+  document.write("lecture");
 }
- function  lectureprocess(){
+function lectureprocess() {
   document.write("lecture process");
- }
+}
 
- function firstParam(x) {
+function firstParam(x) {
   let z = x * x;
-  console.log("params" ,z);
- }
- firstParam(5)
+  console.log("params", z);
+}
+firstParam(5);
 
- let massege =  lecture;
+let massege = lecture;
 
- message= lectureprocess;
- message ()
+message = lectureprocess;
+message();
 
-function shefasebisfunc(price){
-  if (n == 20){
+function shefasebisfunc(price) {
+  if (price == 20) {
     return 50;
   } else {
-    return n * geetfactorial(n-1);
+    return price * shefasebisfunc(price - 1);
+  }
+}
+function getfactorial(n) {
+  // Calculate the factorial of n
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * getfactorial(n - 1);
   }
 }
 
- let  result = getfactorial(4);
-  console.loge(result);
-
-display (50, welcomeMessege);
-
- 
+let result = getfactorial(4);
+console.log(result);
